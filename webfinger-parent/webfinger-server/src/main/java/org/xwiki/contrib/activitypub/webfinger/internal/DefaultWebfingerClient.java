2f53b84d9513cb2186352ee3d2529b86f8bee2e3
Manuel Leduc <manuel.leduc@xwiki.com>
2020-03-18 17:21:18 +0100

    XAP-5: Implements a WebFinger resolution for XWiki
    Integration of an http client for webfinger.

---
+++ webfinger-parent/webfinger-server/src/main/java/org/xwiki/contrib/activitypub/webfinger/internal/DefaultWebfingerClient.java
@@ -0,0 +1,105 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.activitypub.webfinger.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.contrib.activitypub.webfinger.WebfingerClient;
+import org.xwiki.contrib.activitypub.webfinger.WebfingerException;
+import org.xwiki.contrib.activitypub.webfinger.WebfingerJsonParser;
+import org.xwiki.contrib.activitypub.webfinger.entities.JSONResourceDescriptor;
+
+/**
+ * Default implementation of {@link WebfingerClient}.
+ *
+ * @since 1.1
+ * @version $Id$
+ */
+@Component
+@Singleton
+public class DefaultWebfingerClient implements WebfingerClient
+{
+    private static final Pattern ACTOR_REGEX = Pattern.compile("@?(?<username>.+)@(?<domain>.+)");
+
+    private HttpClient httpClient;
+
+    @Inject
+    private WebfingerJsonParser parser;
+
+    /**
+     * Default constructor for {@link DefaultWebfingerClient}.
+     */
+    public DefaultWebfingerClient()
+    {
+        this.httpClient = new HttpClient(new MultiThreadedHttpConnectionManager());
+    }
+
+    @Override
+    public JSONResourceDescriptor get(String webfingerResource) throws WebfingerException
+    {
+        Matcher match = ACTOR_REGEX.matcher(webfingerResource);
+        if (match.matches()) {
+            String username = match.group("username");
+            String domain = match.group("domain");
+            GetMethod get = null;
+            try {
+                String resource = URLEncoder.encode(
+                    String.format("%s@%s", username, domain), "UTF-8");
+                String query = String.format("http://%s/.well-known/webfinger?resource=%s", domain, resource);
+                get = new GetMethod(query);
+                this.httpClient.executeMethod(get);
+
+                InputStream responseBodyAsStream = get.getResponseBodyAsStream();
+                JSONResourceDescriptor parse = this.parser.parse(responseBodyAsStream);
+                return parse;
+            } catch (IOException e) {
+                throw new WebfingerException(
+                    String.format("Error while querying the webfinger resource for %s", webfingerResource), e);
+            } finally {
+                if (get != null) {
+                    get.releaseConnection();
+                }
+            }
+        } else {
+            throw new WebfingerException(String.format("%s is not a valid webfinger resource", webfingerResource),
+                null);
+        }
+    }
+
+    /**
+     * Replace the http client.
+     * @param httpClient An externaly defined http client.
+     */
+    public void setHttpClient(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+}
---
+++ webfinger-parent/webfinger-server/src/test/java/org/xwiki/contrib/activitypub/webfinger/internal/DefaultWebfingerClientTest.java
@@ -0,0 +1,100 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.activitypub.webfinger.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.URIException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.xwiki.contrib.activitypub.webfinger.WebfingerException;
+import org.xwiki.contrib.activitypub.webfinger.WebfingerJsonParser;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.junit5.mockito.MockComponent;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * Test of {@link DefaultWebfingerClient}.
+ *
+ * @since 1.1
+ * @version $Id$
+ */
+@ComponentTest
+class DefaultWebfingerClientTest
+{
+    @InjectMockComponents
+    private DefaultWebfingerClient client;
+
+    @Mock
+    private HttpClient httpClient;
+
+    @MockComponent
+    private WebfingerJsonParser parser;
+
+    @BeforeEach
+    public void setup() throws URISyntaxException
+    {
+        this.client.setHttpClient(this.httpClient);
+    }
+
+    @Test
+    void getQuery() throws Exception
+    {
+        this.client.get("user@test.org");
+        verify(this.httpClient).executeMethod(ArgumentMatchers.argThat(
+            argument -> {
+                try {
+                    return String.valueOf(argument.getURI())
+                               .equals("http://test.org/.well-known/webfinger?resource=user%40test.org");
+                } catch (URIException e) {
+                    return false;
+                }
+            }));
+    }
+
+    @Test
+    void getWebfingerException() throws Exception
+    {
+        when(this.parser.parse(nullable(InputStream.class))).thenThrow(new WebfingerException("TEST", null));
+        WebfingerException actual = assertThrows(WebfingerException.class, () -> this.client.get("user@test.org"));
+        assertEquals("TEST", actual.getMessage());
+    }
+
+    @Test
+    void getIOException() throws Exception
+    {
+        when(this.httpClient.executeMethod(any())).thenThrow(new IOException("TEST"));
+        WebfingerException actual = assertThrows(WebfingerException.class, () -> this.client.get("user@test.org"));
+        assertEquals("Error while querying the webfinger resource for user@test.org", actual.getMessage());
+    }
+}
