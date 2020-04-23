deeef06dc1f5cb0518e629318b7f9d14e9d63f83
Manuel Leduc <manuel.leduc@xwiki.com>
2020-04-23 09:47:10 +0200

    XAP-55: Allow to retrieve messages sent and received
    - Fix AnnounceActivityHandlerTest#handleOutbox()

--- activitypub-server/src/test/java/org/xwiki/contrib/activitypub/internal/activities/AnnounceActivityHandlerTest.java
+++ activitypub-server/src/test/java/org/xwiki/contrib/activitypub/internal/activities/AnnounceActivityHandlerTest.java
@@ -20,18 +20,15 @@ package org.xwiki.contrib.activitypub.internal.activities;/*
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashSet;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.xwiki.contrib.activitypub.ActivityRequest;
-import org.xwiki.contrib.activitypub.entities.AbstractActor;
 import org.xwiki.contrib.activitypub.entities.ActivityPubObjectReference;
 import org.xwiki.contrib.activitypub.entities.Announce;
 import org.xwiki.contrib.activitypub.entities.Document;
-import org.xwiki.contrib.activitypub.entities.Follow;
 import org.xwiki.contrib.activitypub.entities.Inbox;
 import org.xwiki.contrib.activitypub.entities.OrderedCollection;
 import org.xwiki.contrib.activitypub.entities.Outbox;
@@ -41,14 +38,12 @@ import org.xwiki.test.LogLevel;
 import org.xwiki.test.junit5.LogCaptureExtension;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
-import org.xwiki.user.UserReference;
 
 import static java.util.Collections.singleton;
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -88,14 +83,12 @@ class AnnounceActivityHandlerTest extends AbstractHandlerTest
     void handleInbox() throws Exception
     {
         Person sender = new Person()
-                .setPreferredUsername("Followed");
+            .setPreferredUsername("Sender");
 
         ActivityPubObjectReference<Inbox> inboxRef = new ActivityPubObjectReference<>();
         inboxRef.setLink(URI.create("http://inbox/1"));
-        OrderedCollection<AbstractActor> following = new OrderedCollection<>().setName("following");
         Person receiver = new Person()
                 .setPreferredUsername("Receiver")
-                .setFollowing(following.getReference())
                 .setInbox(inboxRef);
 
         ActivityPubObjectReference<OrderedCollection<Announce>> shares = new ActivityPubObjectReference<>();
@@ -110,7 +103,6 @@ class AnnounceActivityHandlerTest extends AbstractHandlerTest
         when(this.activityPubObjectReferenceResolver.resolveReference(announce.getActor())).thenReturn(sender);
         when(this.activityPubObjectReferenceResolver.resolveReference(
                 (ActivityPubObjectReference<Document>) announce.getObject())).thenReturn(document);
-        when(this.activityPubObjectReferenceResolver.resolveReference(receiver.getFollowing())).thenReturn(following);
 
         Inbox inbox = new Inbox();
         when(this.activityPubObjectReferenceResolver.resolveReference(inboxRef)).thenReturn(inbox);
@@ -129,50 +121,42 @@ class AnnounceActivityHandlerTest extends AbstractHandlerTest
         verify(this.notifier).notify(eq(announce), eq(singleton(receiver)));
     }
 
-    @Disabled
     @Test
     void handleOutbox() throws Exception
     {
-        OrderedCollection<AbstractActor> followers = new OrderedCollection<>().setName("followers");
-        UserReference followedRef = mock(UserReference.class);
         ActivityPubObjectReference<Outbox> outboxRef = new ActivityPubObjectReference<>();
         outboxRef.setLink(URI.create("http://outbox/1"));
         Person receiver = new Person()
-                .setPreferredUsername("Receiver")
-                .setFollowers(followers.getReference())
-                .setOutbox(outboxRef);
+            .setPreferredUsername("Receiver")
+            .setOutbox(outboxRef);
 
         Person sender = new Person()
-                .setPreferredUsername("Sender");
-
-        Follow follow = new Follow()
-                .setActor(sender.getReference())
-                .setObject(receiver.getReference());
+            .setPreferredUsername("Sender");
 
         Document document = new Document()
-                .setId(URI.create("http://create/1"))
-                .setContent("Content");
+            .setId(URI.create("http://create/1"))
+            .setContent("Content");
         ProxyActor pa = new ProxyActor(URI.create("http://to/1"));
         Announce announce = new Announce()
-                .setActor(receiver.getReference())
-                .setObject(document)
-                .setId(URI.create("http://announce/1"))
-                .setTo(singletonList(pa));
+            .setActor(receiver.getReference())
+            .setObject(document)
+            .setId(URI.create("http://announce/1"))
+            .setTo(singletonList(pa));
         when(this.activityPubObjectReferenceResolver.resolveReference(announce.getActor())).thenReturn(receiver);
         when(this.activityPubObjectReferenceResolver.resolveReference(
-                (ActivityPubObjectReference<Document>) announce.getObject())).thenReturn(document);
-        when(this.activityPubObjectReferenceResolver.resolveReference(follow.getActor())).thenReturn(sender);
-        when(this.activityPubObjectReferenceResolver.resolveReference(receiver.getFollowers()))
-                .thenReturn(followers);
+            (ActivityPubObjectReference<Document>) announce.getObject())).thenReturn(document);
+        when(this.activityPubObjectReferenceResolver.resolveReference(sender.getReference())).thenReturn(sender);
         Outbox outbox = new Outbox();
         when(this.activityPubObjectReferenceResolver.resolveReference(outboxRef)).thenReturn(outbox);
-        when(this.actorHandler.getXWikiUserReference(receiver)).thenReturn(followedRef);
         Person to = new Person().setName("TO");
         when(this.activityPubObjectReferenceResolver.resolveReference(pa)).thenReturn(to);
         when(this.activityPubObjectReferenceResolver.resolveReference(to.getReference())).thenReturn(to);
+        when(this.activityPubObjectReferenceResolver.resolveTargets(announce))
+            .thenReturn(new HashSet<>(singletonList(to)));
 
         this.handler.handleOutboxRequest(
-                new ActivityRequest<>(receiver, announce, this.servletRequest, this.servletResponse));
+            new ActivityRequest<>(receiver, announce, this.servletRequest, this.servletResponse));
+
         this.verifyResponse(announce);
         verify(this.activityPubStorage).storeEntity(outbox);
         verify(this.activityPubClient).postInbox(to, announce);
