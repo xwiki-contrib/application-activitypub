e898f39154482e3c312541f65a177020248758d0
Manuel Leduc <manuel.leduc@xwiki.com>
2020-12-09 08:52:46 +0100

    XAP-87: Displays the discussions in the dashboard
    - Provides the pages to display the discussions and well as the content

--- activitypub-server/src/main/java/org/xwiki/contrib/activitypub/script/ActivityPubDiscussionsScriptService.java
+++ activitypub-server/src/main/java/org/xwiki/contrib/activitypub/script/ActivityPubDiscussionsScriptService.java
@@ -19,9 +19,10 @@
  */
 package org.xwiki.contrib.activitypub.script;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
@@ -29,10 +30,18 @@ import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.contrib.activitypub.ActivityPubException;
+import org.xwiki.contrib.activitypub.ActivityPubObjectReferenceResolver;
+import org.xwiki.contrib.activitypub.entities.ActivityPubObject;
+import org.xwiki.contrib.activitypub.entities.ActivityPubObjectReference;
+import org.xwiki.contrib.activitypub.entities.ProxyActor;
 import org.xwiki.contrib.discussions.DiscussionContextService;
 import org.xwiki.contrib.discussions.DiscussionService;
 import org.xwiki.contrib.discussions.MessageService;
+import org.xwiki.contrib.discussions.domain.Discussion;
 import org.xwiki.contrib.discussions.domain.DiscussionContext;
+import org.xwiki.contrib.discussions.domain.DiscussionContextEntityReference;
+import org.xwiki.rendering.syntax.Syntax;
+import org.xwiki.wysiwyg.converter.HTMLConverter;
 
 /**
  * Script services for the discussions operations.
@@ -46,6 +55,12 @@ public class ActivityPubDiscussionsScriptService
 {
     private static final String DESCRIPTION_STR = "description";
 
+    private static final String EVENT_STR = "event";
+
+    private static final String ACTIVITYPUB_ACTIVITY_STR = "activitypub-activity";
+
+    private static final String ACTIVITYPUB_ENTITY_TYPE = "activitypub";
+
     @Inject
     private DiscussionService discussionService;
 
@@ -55,63 +70,76 @@ public class ActivityPubDiscussionsScriptService
     @Inject
     private MessageService messageService;
 
+    @Inject
+    private ActivityPubObjectReferenceResolver resolver;
+
+    @Inject
+    private HTMLConverter htmlConverter;
+
     /**
      * Reply to an event by adding a message to a discussion.
      * <p>
      * If the discussion do not exists, or the discussion context, they are created too.
      * <p>
-     * Parameters is a map with three keys:
-     * <ul>
-     *     <li>discussionContexts: a list of maps with four keys: name, description, referenceType, entityReference</li>
-     *     <li>discussion: a map with two keys: title and description</li>
-     *     <li>content: the content of the message</li>
-     * </ul>
      *
-     * @param parameters a map of key values easily usable with Velocity
+     * @param eventId the event id
+     * @param activityId the activity id
+     * @param content the message content
      * @return {@code} true if the operation succeeded, {@code false} otherwise
      */
-    public boolean replyToEvent(Map<String, Object> parameters) throws ActivityPubException
+    public boolean replyToEvent(String eventId, String activityId, String content)
     {
-        List<Object> discussionContextsParamList = (List<Object>) parameters.get("discussionContexts");
-        Map<String, String> discussionMap = (Map<String, String>) parameters.get("discussion");
-        String content = (String) parameters.get("content");
-
-        checkParameters(discussionContextsParamList, discussionMap, content);
-
+        // First search or creates a discussion according to the discussion contexts for the event and the activity of 
+        // the message.
+        DiscussionContext eventDiscussionContext = new DiscussionContext(null, EVENT_STR,
+            EVENT_STR, new DiscussionContextEntityReference(EVENT_STR, eventId));
+        DiscussionContext activityDiscussionContext =
+            new DiscussionContext(null, ACTIVITYPUB_ACTIVITY_STR, ACTIVITYPUB_ACTIVITY_STR,
+                new DiscussionContextEntityReference(ACTIVITYPUB_ENTITY_TYPE, activityId));
         List<DiscussionContext> discussionContexts =
-            initializeDiscussionContexts(discussionContextsParamList);
-        return this.discussionService
-            .getOrCreate(discussionMap.get("title"), discussionMap.get(DESCRIPTION_STR),
-                discussionContexts.stream().map(DiscussionContext::getReference).collect(Collectors.toList()))
-            .map(it -> this.messageService.create(content, it.getReference()).isPresent())
+            initializeDiscussionContexts(eventDiscussionContext, activityDiscussionContext);
+        String discussionTitle = String.format("Discussion for %s", activityId);
+        List<String> discussionContextReferences =
+            discussionContexts.stream().map(DiscussionContext::getReference).collect(Collectors.toList());
+        Optional<Discussion> discussionOpt = this.discussionService.getOrCreate(discussionTitle, discussionTitle,
+            discussionContextReferences);
+        discussionOpt.ifPresent(discussion -> {
+
+            // Link the actors as discussion context of discussion.
+            try {
+                ActivityPubObject object =
+                    this.resolver.resolveReference(new ActivityPubObjectReference<>().setLink(URI.create(activityId)));
+                for (ProxyActor activityPubObjectReference : object.getTo()) {
+                    ActivityPubObject object1 = this.resolver.resolveReference(activityPubObjectReference);
+                    this.discussionContextService
+                        .getOrCreate(object1.getName(), object1.getName(), ACTIVITYPUB_ENTITY_TYPE,
+                            object1.getId().toASCIIString())
+                        .ifPresent(
+                            discussionContext -> this.discussionContextService.link(discussionContext, discussion));
+                }
+            } catch (ActivityPubException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // Finally, creates the message.
+        return discussionOpt
+            // TODO: take into account the syntax and check if the conversion is required
+            .map(it -> this.messageService
+                .create(this.htmlConverter.fromHTML(content, Syntax.XWIKI_2_1.toIdString()), it.getReference())
+                .isPresent())
             .orElse(false);
     }
 
-    private void checkParameters(List<Object> discussionContextsParamList, Map<String, String> discussionMap,
-        String content)
-        throws ActivityPubException
-    {
-        if (discussionContextsParamList == null) {
-            throw new ActivityPubException("Parameter [discussionContexts] missing.");
-        }
-
-        if (discussionMap == null) {
-            throw new ActivityPubException("Parameter [discussion] missing.");
-        }
-
-        if (content == null) {
-            throw new ActivityPubException("Parameter [content] missing.");
-        }
-    }
-
-    private List<DiscussionContext> initializeDiscussionContexts(List<Object> discussionContextsParamList)
+    private List<DiscussionContext> initializeDiscussionContexts(DiscussionContext... discussionContextsIns)
     {
         List<DiscussionContext> discussionContexts = new ArrayList<>();
-        for (Object discussionContextParam : discussionContextsParamList) {
-            Map<String, String> discussionContext = (Map<String, String>) discussionContextParam;
-            this.discussionContextService
-                .getOrCreate(discussionContext.get("name"), discussionContext.get(DESCRIPTION_STR),
-                    discussionContext.get("referenceType"), discussionContext.get("entityReference"))
+        for (DiscussionContext discussionContextsIn : discussionContextsIns) {
+            String name = discussionContextsIn.getName();
+            String description = discussionContextsIn.getDescription();
+            String type = discussionContextsIn.getEntityReference().getType();
+            String reference = discussionContextsIn.getEntityReference().getReference();
+            this.discussionContextService.getOrCreate(name, description, type, reference)
                 .ifPresent(discussionContexts::add);
         }
         return discussionContexts;
--- activitypub-server/src/test/java/org/xwiki/contrib/activitypub/script/ActivityPubDiscussionsScriptServiceTest.java
+++ activitypub-server/src/test/java/org/xwiki/contrib/activitypub/script/ActivityPubDiscussionsScriptServiceTest.java
@@ -19,29 +19,32 @@
  */
 package org.xwiki.contrib.activitypub.script;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.xwiki.contrib.activitypub.ActivityPubException;
+import org.xwiki.contrib.activitypub.ActivityPubObjectReferenceResolver;
+import org.xwiki.contrib.activitypub.entities.ActivityPubObject;
+import org.xwiki.contrib.activitypub.entities.ProxyActor;
 import org.xwiki.contrib.discussions.DiscussionContextService;
 import org.xwiki.contrib.discussions.DiscussionService;
 import org.xwiki.contrib.discussions.MessageService;
 import org.xwiki.contrib.discussions.domain.Discussion;
 import org.xwiki.contrib.discussions.domain.DiscussionContext;
+import org.xwiki.contrib.discussions.domain.DiscussionContextEntityReference;
 import org.xwiki.contrib.discussions.domain.Message;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
 import org.xwiki.test.junit5.mockito.MockComponent;
+import org.xwiki.wysiwyg.converter.HTMLConverter;
 
 import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -65,91 +68,54 @@ class ActivityPubDiscussionsScriptServiceTest
     @MockComponent
     private MessageService messageService;
 
+    @MockComponent
+    private ActivityPubObjectReferenceResolver resolver;
+
+    @MockComponent
+    private HTMLConverter htmlConverter;
+
+
     @Test
     void replyToEvent() throws Exception
     {
-        Map<String, Object> parameters = new HashMap<>();
-        Map<Object, Object> discussionContext1 = new HashMap<>();
-        discussionContext1.put("name", "discussionContext1Name");
-        discussionContext1.put("description", "discussionContext1Description");
-        discussionContext1.put("referenceType", "discussionContext1Type");
-        discussionContext1.put("entityReference", "discussionContext1Ref");
-        Map<Object, Object> discussionContext2 = new HashMap<>();
-        discussionContext2.put("name", "discussionContext2Name");
-        discussionContext2.put("description", "discussionContext2Description");
-        discussionContext2.put("referenceType", "discussionContext2Type");
-        discussionContext2.put("entityReference", "discussionContext2Ref");
-        List<Object> discussionContexts = asList(discussionContext1, discussionContext2);
-
-        Map<Object, Object> discussion = new HashMap<>();
-        discussion.put("title", "discussionTitle");
-        discussion.put("description", "discussionDescription");
-
-        parameters.put("discussionContexts", discussionContexts);
-        parameters.put("discussion", discussion);
-        parameters.put("content", "messageContent");
-
         DiscussionContext dc1 = new DiscussionContext("dc1", "discussionContext1Name",
             "discussionContext1Description",
-            "discussionContext1Type",
-            "discussionContext1Ref");
-        when(this.discussionContextService
-            .getOrCreate("discussionContext1Name", "discussionContext1Description", "discussionContext1Type",
-                "discussionContext1Ref"))
-            .thenReturn(Optional.of(dc1));
+            new DiscussionContextEntityReference("discussionContext1Type",
+                "discussionContext1Ref"));
         DiscussionContext dc2 = new DiscussionContext("dc2", "discussionContext2Name",
             "discussionContext2Description",
-            "discussionContext2Type",
-            "discussionContext2Ref");
+            new DiscussionContextEntityReference("discussionContext2Type",
+                "discussionContext2Ref"));
+        ActivityPubObject mock = mock(ActivityPubObject.class);
+        ProxyActor mock1 = mock(ProxyActor.class);
+
+        when(this.discussionContextService.getOrCreate("event", "event", "event", "discussionContext1Ref"))
+            .thenReturn(Optional.of(dc1));
         when(this.discussionContextService
-            .getOrCreate("discussionContext2Name", "discussionContext2Description", "discussionContext2Type",
+            .getOrCreate("activitypub-activity", "activitypub-activity", "activitypub",
                 "discussionContext2Ref"))
             .thenReturn(Optional.of(dc2));
 
-        Discussion d = new Discussion("d1", "discussionTitle", "discussionDescription");
-        when(this.discussionService.getOrCreate("discussionTitle", "discussionDescription",
-            asList(dc1.getReference(), dc2.getReference())))
+        Discussion d = new Discussion("d1", "discussionTitle", "discussionDescription", new Date());
+        when(this.discussionService
+            .getOrCreate("Discussion for discussionContext2Ref", "Discussion for discussionContext2Ref",
+                asList(dc1.getReference(), dc2.getReference())))
             .thenReturn(Optional.of(d));
+
+        when(this.resolver.resolveReference(any())).thenReturn(mock);
+        when(mock.getTo()).thenReturn(singletonList(mock1));
+        ActivityPubObject mock2 = mock(ActivityPubObject.class);
+        when(this.resolver.resolveReference(mock1)).thenReturn(mock2);
+        when(mock2.getId()).thenReturn(URI.create("http://servr/path"));
+        
 
         when(this.messageService.create("messageContent", d.getReference())).thenReturn(
             Optional.of(new Message("mr", "messageContent", "user", "actorRef", new Date(), new Date(), d)));
 
-        boolean b = this.activityPubDiscussionsScriptService.replyToEvent(parameters);
+        when(this.htmlConverter.fromHTML(eq("messageContent"), any())).thenReturn("messageContent");
+
+        boolean b = this.activityPubDiscussionsScriptService
+            .replyToEvent("discussionContext1Ref", "discussionContext2Ref", "messageContent");
         assertTrue(b);
-    }
-
-    @Test
-    void replyToEventDiscussionContextsMissing()
-    {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("wrong", null);
-
-        ActivityPubException activityPubException = assertThrows(ActivityPubException.class,
-            () -> this.activityPubDiscussionsScriptService.replyToEvent(parameters));
-        assertEquals("Parameter [discussionContexts] missing.", activityPubException.getMessage());
-    }
-
-    @Test
-    void replyToEventDiscussionMissing()
-    {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("discussionContexts", Arrays.asList());
-        parameters.put("wrong", new HashMap<>());
-
-        ActivityPubException activityPubException = assertThrows(ActivityPubException.class,
-            () -> this.activityPubDiscussionsScriptService.replyToEvent(parameters));
-        assertEquals("Parameter [discussion] missing.", activityPubException.getMessage());
-    }
-    
-    @Test
-    void replyToEventContextMissing()
-    {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("discussionContexts", Arrays.asList());
-        parameters.put("discussion", new HashMap<>());
-
-        ActivityPubException activityPubException = assertThrows(ActivityPubException.class,
-            () -> this.activityPubDiscussionsScriptService.replyToEvent(parameters));
-        assertEquals("Parameter [content] missing.", activityPubException.getMessage());
     }
 }
